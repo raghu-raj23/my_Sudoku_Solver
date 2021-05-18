2ccca36d9ae20d7e06a0148ad6483738385225d4
raghu-raj23 <raghuraj.muni@gmail.com>
2021-05-18 18:27:14 +0530

    updated readme, visualizing the board

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# my_Sudoku_Solver
-This repo is to implement an automatic Sudoku solver using algorithms
+## my_Sudoku_Solver
+- This repo is to implement an automatic Sudoku solver using algorithms.
+
+#### So what is Sudoku?
+- Sudoku is a logic-based, combinatorial number-placement puzzle. In classic sudoku, the objective is to fill a 9×9 grid with digits so that each column, each row, and each of the nine 3×3 subgrids that compose the grid contains all the digits from 1 to 9.
+### Steps to create the automatic sudoku solver:
+##### Naive Algorithm:
+- try out all the numbers for all the cells(This is very long).
+##### With backtracking:
+- [X] pick an empty cell
+- [X] try all numbers for the cell
+- [X] find one that works
+- [X] repeat the above steps
+- [X] backtrack when no correct ans
---
+++ sudoku.py
@@ -0,0 +1,42 @@
+game_board = [
+    [8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 6, 0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 9, 0, 2, 0, 0],
+    [0, 5, 0, 0, 0, 7, 0, 0, 0],
+    [0, 0, 0, 0, 4, 5, 7, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 3, 0],
+    [0, 0, 1, 0, 0, 0, 0, 6, 8],
+    [0, 0, 8, 5, 0, 0, 0, 1, 0],
+    [0, 9, 0, 0, 0, 0, 4, 0, 0]
+]
+
+
+def is_valid(board, num, pos):
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return i, j  # row, col
